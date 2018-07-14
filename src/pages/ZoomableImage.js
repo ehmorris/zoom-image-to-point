360db3f8098ba25278e740c377dbf60d8b1e9876
Edwin Morris <edwin@rapgenius.com>
2018-07-14 17:39:28 -0400

    Simple zoom

--- src/pages/ZoomableImage.js
+++ src/pages/ZoomableImage.js
@@ -2,13 +2,45 @@ import React from "react"
 import Drake from "../images/Drake.jpg"
 
 class ZoomableImage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.zoom = this.zoom.bind(this);
+
+    this.state = {
+      x: 0,
+      y: 0,
+      width: null,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      width: window.innerWidth,
+    });
+  }
+
+  zoom(event) {
+    const newWidth = this.state.width + event.deltaY;
+
+    this.setState({
+      width: newWidth,
+    });
+
+    event.preventDefault();
+  }
+
   render() {
     return (
       <img
-        style={{
-          width: '100%',
-        }}
+        onWheel={this.zoom}
         src={Drake}
+        style={{
+          position: 'absolute',
+          left: `${this.state.x}px`,
+          top: `${this.state.y}px`,
+          width: `${this.state.width}px`,
+        }}
       />
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,4 @@
 import React from "react"
 import ZoomableImage from "./ZoomableImage"
-import CenterContent from "./CenterContent"
 
-export default () => {
-  return (
-    <CenterContent>
-      <ZoomableImage />
-    </CenterContent>
-  )
-}
+export default () => <ZoomableImage />
