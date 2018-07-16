0d0bdc404eccec87a570abe6a82da3d3d0a0dfbd
Edwin Morris <edwin@rapgenius.com>
2018-07-16 10:23:48 -0400

    Stub image scaling, not quite working
    Probably because mouse coordinates are relative to the document and not
    to the image. We need to take the image's x/y into account when we
    calculate the mouse position

--- src/pages/ZoomableImage.js
+++ src/pages/ZoomableImage.js
@@ -3,37 +3,84 @@ import Drake from "../images/Drake.jpg"
 
 class ZoomableImage extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.zoom = this.zoom.bind(this);
+    this.zoom = this.zoom.bind(this)
+    this.imageLoaded = this.imageLoaded.bind(this)
+    this.image = React.createRef()
 
     this.state = {
       x: 0,
       y: 0,
       width: null,
+      aspectRatio: null,
     }
   }
 
-  componentDidMount() {
+  imageLoaded() {
+    const {width, height} = this.image.current.getBoundingClientRect()
+
     this.setState({
       width: window.innerWidth,
-    });
+      aspectRatio: height/width,
+    })
+  }
+
+  mousePositionRelativeToImage(event) {
+    const imageHeight = this.state.width * this.state.aspectRatio
+
+    return {
+      mouseOffsetX: event.clientX,
+      mouseOffsetY: event.clientY,
+      mouseXPercent: event.clientX / this.state.width,
+      mouseYPercent: event.clientY / imageHeight,
+    }
   }
 
   zoom(event) {
-    const newWidth = this.state.width + event.deltaY;
+    const {mouseOffsetX, mouseOffsetY, mouseXPercent, mouseYPercent} =
+      this.mousePositionRelativeToImage(event)
+
+    const oldWidth = this.state.width
+    const oldHeight = this.state.width * this.state.aspectRatio
+
+    const newWidth = oldWidth + event.deltaY
+    const newHeight = newWidth * this.state.aspectRatio
+
+    const newXDiff = (mouseXPercent * newWidth) - mouseOffsetX
+    const newYDiff = (mouseYPercent * newHeight) - mouseOffsetY
+
+    const newX = this.state.x - newXDiff
+    const newY = this.state.y - newYDiff
+
+    console.group()
+    console.log(`oldWidth: ${oldWidth}`)
+    console.log(`oldHeight: ${oldHeight}`)
+    console.log(`mouseOffsetX: ${mouseOffsetX}`)
+    console.log(`mouseOffsetY: ${mouseOffsetY}`)
+    console.log(`newWidth: ${newWidth}`)
+    console.log(`newHeight: ${newHeight}`)
+    console.log(`newXDiff: ${newXDiff}`)
+    console.log(`newYDiff: ${newYDiff}`)
+    console.log(`newX: ${newX}`)
+    console.log(`newY: ${newY}`)
+    console.groupEnd()
 
     this.setState({
       width: newWidth,
-    });
+      x: newX,
+      y: newY,
+    })
 
-    event.preventDefault();
+    event.preventDefault()
   }
 
   render() {
     return (
       <img
         onWheel={this.zoom}
+        onLoad={this.imageLoaded}
+        ref={this.image}
         src={Drake}
         alt=""
         style={{
@@ -47,4 +94,4 @@ class ZoomableImage extends React.Component {
   }
 }
 
-export default ZoomableImage;
+export default ZoomableImage
